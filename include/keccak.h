cbcb1f0d7bb9aa3dbfcde746d97c0aa49946adeb
Derrity <86703464+Derrity@users.noreply.github.com>
2024-04-20 00:11:41 +0800

    Delete include/keccak.h

--- include/keccak.h
+++
@@ -1,6 +0,0 @@
-#ifndef BITADDRESS_KECCAK_H
-#define BITADDRESS_KECCAK_H
-#include <iostream>
-void keccak(const uint8_t *in, int inlen, uint8_t *md, int mdlen);
-void keccak_256(const uint8_t *in, int inlen, uint8_t *md);
-#endif //BITADDRESS_KECCAK_H
